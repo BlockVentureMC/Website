5318361ca14f0d1521196017c2a2e55757f5da0c
InvalidJokerDE <0limitdev@gmail.com>
2024-04-27 19:28:30 +0200

    feat: initial pages

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <header>
+      <h1 class="text-3xl font-bold underline">BlockVenture</h1>
+    </header>
+    <slot />
+  </div>
+</template>
+<script setup lang="ts">
+</script>
