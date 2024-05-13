4c7651472b21922612732f5935ae86d80a5cec6b
InvalidJokerDE <0limitdev@gmail.com>
2024-05-13 17:36:53 +0200

    add opus

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,15 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
   plugins: ["~/plugins/websocket.ts"],
+  nitro: {
+    experimental: {
+      websocket: true,
+    }
+  },
+  ssr: false,
+  runtimeConfig: {
+    public: {
+      wsUrl: process.env.WS_URL,
+    }
+  }
 });
--- pages/audio/index.vue
+++ pages/audio/index.vue
@@ -1,9 +1,63 @@
 <script setup lang="ts">
+
 definePageMeta({
   layout: 'audio',
 })
+
+import {OpusDecoderWebWorker} from 'opus-decoder';
+//import WebSocket from 'ws';
+
+
+function start() {
+  const socket = new WebSocket('ws://localhost:3000/api/audio');
+
+  socket.binaryType = 'arraybuffer';
+  socket.onopen = (event) => {
+    console.log('Connected to audio websocket');
+  };
+
+  socket.onerror = (event) => {
+    console.log(event);
+
+    console.log('Error connecting to audio websocket');
+  };
+
+  const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+  const sampleRate = audioCtx.sampleRate;
+
+  socket.onmessage = async (event) => {
+    console.log('Received message: ' + event.data);
+    const data = event.data;
+    console.log('Received data: ' + data);
+
+
+    const decoder = new OpusDecoderWebWorker({ channels: 2 });
+
+    await decoder.ready;
+    const decoded = await decoder.decodeFrame(data);
+    console.log('Decoded data: ' + decoded);
+
+    // play audio
+    const audioCtx = new AudioContext();
+    const source = audioCtx.createBufferSource();
+    const buffer = audioCtx.createBuffer(2, decoded.samplesDecoded, sampleRate);
+
+    for (let channel = 0; channel < 2; channel++) {
+      buffer.copyToChannel(decoded.channelData[channel], channel);
+    }
+    source.buffer = buffer;
+    source.connect(audioCtx.destination);
+    source.start();
+  };
+
+  socket.onclose = (event) => {
+    console.log('Disconnected from audio websocket');
+  };
+}
+
 </script>
 
 <template>
   <h1>Test Audio</h1>
+  <button @click="start">Start</button>
 </template>
